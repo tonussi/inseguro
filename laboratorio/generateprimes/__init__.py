52384cf7cfa885b6d3fee52cf392df04022dfb08
tonussi <lptonussi@gmail.com>
2015-05-30 17:16:34 -0300

    Alimenta uma vez apenas

--- laboratorio/generateprimes/__init__.py
+++ laboratorio/generateprimes/__init__.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-from pseudo import PseudoRandomGenerator
+from pseudo import PrimeTester, Twister
 import subprocess as sub
 import sys, math
 
-ACURACIDADE = 25
 BITS = int(math.log10(10 ** 100) / math.log10(2)) + 1  # 333 BITS
 BYTES = int((int(math.log10(10 ** 100) / math.log10(2)) + 1) / 8) + 1  # 42 BYTES
 IO = open('stderr', 'w')
@@ -11,15 +10,16 @@ IO = open('stderr', 'w')
 
 def dd():
     try:
-        comandos = ['dd', 'if=/dev/urandom', 'of=./entropy', 'count=42', 'bs=1']
+        comandos = ['dd', 'if=/dev/urandom', 'of=./entropy', 'count=1', 'bs=8']
         a = sub.Popen(comandos, stderr=IO)
         a.communicate()
     except IOError:
         print(IOError)
         sys.exit()
+    finally:
+        sub._cleanup()
 
-
-def ler_arquivo(nome):
+def lerarquivo(nome):
     try:
         plain = ''
         arq = open(nome, 'rb')
@@ -34,31 +34,39 @@ def ler_arquivo(nome):
 
 
 def main():
-    pseudo = PseudoRandomGenerator()
+    if len(sys.argv) > 2:
+        QUANTIDADE, ACURACIDADE = eval(sys.argv[1]), eval(sys.argv[2])
+    else:
+        QUANTIDADE, ACURACIDADE = 10, 25
+
+    primestester = PrimeTester()
+    pseudo = Twister()
+
     rejeitados_miller_rabin = 0
     rejeitados_solovay_strassen = 0
     dez_primos_miller_rabin = []
     dez_primos_solovay_strassen = []
 
-    while len(dez_primos_miller_rabin) < 10:
-        dd()
-        n = ler_arquivo('entropy')
-        pseudo.alimentar_gerador(int(n))
-        candidato = pseudo.extract_number() | 0x1
+    dd()
+    n = lerarquivo('entropy')
+    pseudo.alimentar(int(n))
 
-        p = pseudo.miller_rabin(candidato, ACURACIDADE)
+    while len(dez_primos_miller_rabin) < QUANTIDADE:
+        candidato = pseudo.extrair() | 0x1
+
+        p = primestester.miller_rabin(candidato, ACURACIDADE)
         if p[1]:
             dez_primos_miller_rabin.append(('http://www.wolframalpha.com/input/?i=' + str(candidato) + '+prime?', p[1]))
             print('Valor', candidato, 'primo (Miller-Rabin) ?', p, 'Rejeitados:', rejeitados_miller_rabin)
         rejeitados_miller_rabin += 1
 
-    while len(dez_primos_solovay_strassen) < 10:
-        dd()
-        n = ler_arquivo('entropy')
-        pseudo.alimentar_gerador(int(n))
-        candidato = pseudo.extract_number() | 0x1
+    while len(dez_primos_solovay_strassen) < QUANTIDADE:
+        #dd()
+        #n = lerarquivo('entropy')
+        #twister.alimentar(int(n))
+        candidato = pseudo.extrair() | 0x1
 
-        q = pseudo.solovay_strassen(candidato, ACURACIDADE)
+        q = primestester.solovay_strassen(candidato, ACURACIDADE)
         if q[1]:
             dez_primos_solovay_strassen.append(
                 ('http://www.wolframalpha.com/input/?i=' + str(candidato) + '+prime?', p[1]))
@@ -77,9 +85,12 @@ def main():
     print('\nTotal Rejeitados Miller-Rabin:', rejeitados_miller_rabin,
           '\nTotal Rejeitados Solovay-Strassen:', rejeitados_solovay_strassen)
     print('\nDados:')
-    print('Rejeitados Miller-Rabin/Rejeitados Solovay-Strassen:', rejeitados_miller_rabin / rejeitados_solovay_strassen)
-    print('Taxa de Acertos=10/Rejeitos Miller-Rabin:', 10 / rejeitados_miller_rabin)
-    print('Taxa de Acertos=10/Rejeitos Solovay-Strassen:', 10 / rejeitados_solovay_strassen)
+    print('Taxa Rejeitados Miller-Rabin/Rejeitados Solovay-Strassen:',
+          rejeitados_miller_rabin / rejeitados_solovay_strassen)
+    print('\nMiller-Rabin rejeita:', (rejeitados_miller_rabin / rejeitados_solovay_strassen) * 100, '%',
+          'em relacao a Solovay-Strassen')
+    print('Taxa de Acertos=', QUANTIDADE, '/', 'Rejeitos Miller-Rabin:', 10 / rejeitados_miller_rabin)
+    print('Taxa de Acertos=', QUANTIDADE, '/', 'Rejeitos Solovay-Strassen:', 10 / rejeitados_solovay_strassen)
 
 
 if __name__ == "__main__":
